7d9776a75d54ac86ca32e4703f4de7114fe84a84
Hannah Schellekens <github@hannahschellekens.nl>
2024-10-12 20:09:31 +0200

    Gave Heart Breaker heart bullet pattern.

--- src/main/kotlin/maliwan/mcbl/weapons/gun/behaviour/shotgun/HeartBreaker.kt
+++ src/main/kotlin/maliwan/mcbl/weapons/gun/behaviour/shotgun/HeartBreaker.kt
@@ -1,28 +1,45 @@
 package maliwan.mcbl.weapons.gun.behaviour.shotgun
 
+import maliwan.mcbl.util.Probability
+import maliwan.mcbl.util.modifyRandom
 import maliwan.mcbl.weapons.gun.*
-import maliwan.mcbl.weapons.gun.behaviour.DefaultPrefixProvider
-import maliwan.mcbl.weapons.gun.behaviour.PostGenerationBehaviour
-import maliwan.mcbl.weapons.gun.behaviour.UniqueGun
-import maliwan.mcbl.weapons.gun.behaviour.UpdateAssemblyBehaviour
+import maliwan.mcbl.weapons.gun.behaviour.*
 import maliwan.mcbl.weapons.gun.parts.Capacitor
 import maliwan.mcbl.weapons.gun.parts.ShotgunParts
+import maliwan.mcbl.weapons.gun.pattern.RandomBulletPattern
 
 /**
  * @author Hannah Schellekens
  */
-open class HeartBreaker : UniqueGun, PostGenerationBehaviour, DefaultPrefixProvider, UpdateAssemblyBehaviour {
+open class HeartBreaker(scale: Double = 0.02) : UniqueGun, PostGenerationBehaviour, DefaultPrefixProvider,
+    UpdateAssemblyBehaviour, BulletPatternProvider {
 
     override val baseName = "Heart Breaker"
     override val redText = "I don't want to set the world on\nfire..."
     override val defaultPrefix = "Miss Moxxi's"
+    override val bulletPatternProcessor = RandomBulletPattern.of(
+        scale to 0.0,
+        2.0 * scale to 1.0 * scale,
+        2.0 * scale to 2.0 * scale,
+        scale to 3.0 * scale,
+        0.0 to 3.0 * scale,
+        -scale to 2.0 * scale,
+        -2.0 * scale to scale,
+        -3.0 * scale to 0.0,
+        -2.0 * scale to -scale,
+        -scale to -2.0 * scale,
+        0.0 to -3.0 * scale,
+        scale to -3.0 * scale,
+        2.0 * scale to -2.0 * scale,
+        2.0 * scale to -scale
+    )
 
     override fun onFinishGeneration(properties: GunProperties, assembly: WeaponAssembly) {
         ShotgunParts.Barrel.HYPERION.applyStatModifiers(properties)
         statModifiers.applyAll(properties)
 
-        // TODO: Add Heart shaped bullet pattern.
         properties.pelletCount = 10
+        properties.accuracy = Probability(0.75.modifyRandom(0.04))
     }
 
     override fun updateAssembly(assembly: WeaponAssembly): WeaponAssembly {
